ec612dd7584ded8d315daf41009bdb8a8e2b8243
philgagnon12 <philgagnon12@gmail.com>
2020-04-22 08:18:17 -0400

    system tests

--- cpfphigConfig.in.cmake
+++ cpfphigConfig.in.cmake
@@ -1,4 +1,7 @@
 @PACKAGE_INIT@
 
 include(CMakeFindDependencyMacro)
-#find_dependency( Threads::Threads)
+
+find_dependency( Threads )
+
+include(@CMAKE_CURRENT_BINARY_DIR@/@CMAKE_PROJECT_NAME@.cmake)

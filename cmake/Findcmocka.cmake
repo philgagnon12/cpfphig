1954482b7a47e9734a32181fec80f6833c4f33c2
philgagnon12 <philgagnon12@gmail.com>
2020-05-23 14:13:46 -0400

    store downloaded cmocka in vendor

--- cmake/Findcmocka.cmake
+++ cmake/Findcmocka.cmake
@@ -1,23 +1,33 @@
 include(FetchContent)
 
-FetchContent_Declare( cmocka
-    GIT_REPOSITORY "https://git.cryptomilk.org/projects/cmocka.git"
-    GIT_TAG        "cmocka-1.1.5"
-)
+if( NOT EXISTS "${PROJECT_SOURCE_DIR}/vendor/cmocka" )
+    FetchContent_Declare( cmocka
+        GIT_REPOSITORY "https://git.cryptomilk.org/projects/cmocka.git"
+        GIT_TAG        "cmocka-1.1.5"
+        SOURCE_DIR     "${PROJECT_SOURCE_DIR}/vendor/cmocka"
+    )
+    message( INFO "Downloaded https://git.cryptomilk.org/projects/cmocka.git" )
+else()
+    FetchContent_Declare( cmocka
+        URL            "${PROJECT_SOURCE_DIR}/vendor/cmocka"
+    )
+endif()
 
 FetchContent_GetProperties( cmocka )
+
+if(WIN32)
+    set(LIBRARY_PREFIX "")
+    set(LIBRARY_SUFFIX ${CMAKE_LINK_LIBRARY_SUFFIX})
+else()
+    set(LIBRARY_PREFIX ${CMAKE_SHARED_LIBRARY_PREFIX})
+    set(LIBRARY_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
+endif()
 
 if( NOT cmocka_POPULATED )
     FetchContent_Populate( cmocka )
     FetchContent_GetProperties( cmocka )
 
-    if(WIN32)
-        set(LIBRARY_PREFIX "")
-        set(LIBRARY_SUFFIX ${CMAKE_LINK_LIBRARY_SUFFIX})
-    else()
-        set(LIBRARY_PREFIX ${CMAKE_SHARED_LIBRARY_PREFIX})
-        set(LIBRARY_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX})
-    endif()
+    set( LIBRARY_PATH "${cmocka_BINARY_DIR}/lib/${LIBRARY_PREFIX}cmocka${LIBRARY_SUFFIX}" )
 
     add_custom_command( OUTPUT "${cmocka_BINARY_DIR}/CMakeCache.txt"
         COMMAND ${CMAKE_COMMAND} ${cmocka_SOURCE_DIR} -G "${CMAKE_GENERATOR}" -DCMAKE_INSTALL_PREFIX="${cmocka_BINARY_DIR}" -DBUILD_SHARED_LIBS=YES
@@ -29,20 +39,20 @@ if( NOT cmocka_POPULATED )
         DEPENDS "${cmocka_BINARY_DIR}/CMakeCache.txt"
     )
 
-add_custom_command( OUTPUT "${cmocka_BINARY_DIR}/lib/${LIBRARY_PREFIX}cmocka${LIBRARY_SUFFIX}"
+    add_custom_command( OUTPUT "${LIBRARY_PATH}"
         COMMAND ${CMAKE_COMMAND} --build ${cmocka_BINARY_DIR} --target install
         WORKING_DIRECTORY ${cmocka_BINARY_DIR}
         COMMENT "build libcmocka"
     )
 
     add_custom_target( buildlibcmocka
-        DEPENDS "${cmocka_BINARY_DIR}/lib/${LIBRARY_PREFIX}cmocka${LIBRARY_SUFFIX}"
+        DEPENDS "${LIBRARY_PATH}"
     )
 
     add_library(libcmocka SHARED IMPORTED GLOBAL)
-    set_target_properties(libcmocka PROPERTIES IMPORTED_LOCATION "${cmocka_BINARY_DIR}/lib/${LIBRARY_PREFIX}cmocka${LIBRARY_SUFFIX}")
+    set_target_properties(libcmocka PROPERTIES IMPORTED_LOCATION "${LIBRARY_PATH}")
     # Next line applies to windows only
-    set_target_properties(libcmocka PROPERTIES IMPORTED_IMPLIB "${cmocka_BINARY_DIR}/lib/${LIBRARY_PREFIX}cmocka${LIBRARY_SUFFIX}")
+    set_target_properties(libcmocka PROPERTIES IMPORTED_IMPLIB "${LIBRARY_PATH}")
     add_dependencies( buildlibcmocka configurelibcmocka )
     add_dependencies( libcmocka buildlibcmocka )
 endif()

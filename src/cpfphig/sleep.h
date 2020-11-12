41ddcfe25f3eb481f25028def79ce53df8526a65
philgagnon12 <philgagnon12@gmail.com>
2020-11-12 17:16:25 -0500

    c++ sleep

--- src/cpfphig/sleep.h
+++ src/cpfphig/sleep.h
@@ -3,8 +3,18 @@
 
 #include "cpfphig/cpfphig.h"
 
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+
 cpfphig
 cpfphig_sleep( int                                      Milliseconds,
                CPFPHIG_OPTIONAL struct cpfphig_error*   Error );
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
